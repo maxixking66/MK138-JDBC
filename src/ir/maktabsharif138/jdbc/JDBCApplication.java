5087aa5787df9ea645c839f33a1f499e0082434b
maxixking66 <maxixking66@gamil.com>
2025-11-28 10:07:23 +0330

    feat: add select query example

--- src/ir/maktabsharif138/jdbc/JDBCApplication.java
+++ src/ir/maktabsharif138/jdbc/JDBCApplication.java
@@ -3,7 +3,9 @@ package ir.maktabsharif138.jdbc;
 import ir.maktabsharif138.jdbc.util.ApplicationContext;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCApplication {
 
@@ -11,6 +13,25 @@ public class JDBCApplication {
         ApplicationContext ctx = ApplicationContext.getInstance();
         Connection connection = ctx.getConnection();
 
+        Statement statement = connection.createStatement();
+        String sql = "select * from users";
+//        String sql = "select id as u_id, username, age from users";
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()) {
+//            resultSet.getXXX()
+            int userId = resultSet.getInt("id");
+//            int userId = resultSet.getInt(1);
+            String username = resultSet.getString(2);
+//            String username = resultSet.getString("username");
+            int age = resultSet.getInt("age");
+
+            System.out.format("user information -> id: %d, username: %s, age: %d", userId, username, age);
+            System.out.println();
+        }
+
+
+        statement.close();
         connection.close();
     }
 }
