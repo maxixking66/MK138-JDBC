96a485ef367047b52a007bafc52e3569edaf3279
maxixking66 <maxixking66@gamil.com>
2025-11-28 09:45:35 +0330

    feat:  add ApplicationProperties and get connection in main class

--- src/ir/maktabsharif138/jdbc/JDBCApplication.java
+++ src/ir/maktabsharif138/jdbc/JDBCApplication.java
@@ -1,8 +1,22 @@
 package ir.maktabsharif138.jdbc;
 
+import ir.maktabsharif138.jdbc.util.ApplicationProperties;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCApplication {
 
-    static void main() {
+    static void main() throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                ApplicationProperties.DATABASE_URL,
+                ApplicationProperties.DATABASE_USER,
+                ApplicationProperties.DATABASE_PASSWORD
+        );
 
+        System.out.println();
+
+        connection.close();
     }
 }
---
+++ src/ir/maktabsharif138/jdbc/util/ApplicationProperties.java
@@ -0,0 +1,12 @@
+package ir.maktabsharif138.jdbc.util;
+
+public class ApplicationProperties {
+
+    public static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/postgres?currentSchema=mk_138";
+    public static final String DATABASE_USER = "postgres";
+    public static final String DATABASE_PASSWORD = "123456789";
+
+    private ApplicationProperties() {
+    }
+
+}
