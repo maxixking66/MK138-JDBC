6e69d29625f8fe241cda505a9189a11f3cc800be
maxixking66 <maxixking66@gamil.com>
2025-11-28 10:11:36 +0330

    feat: add user class and map resultSet to it

--- src/ir/maktabsharif138/jdbc/JDBCApplication.java
+++ src/ir/maktabsharif138/jdbc/JDBCApplication.java
@@ -1,11 +1,14 @@
 package ir.maktabsharif138.jdbc;
 
+import ir.maktabsharif138.jdbc.domains.User;
 import ir.maktabsharif138.jdbc.util.ApplicationContext;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JDBCApplication {
 
@@ -14,22 +17,23 @@ public class JDBCApplication {
         Connection connection = ctx.getConnection();
 
         Statement statement = connection.createStatement();
-        String sql = "select * from users";
+        String sql = "select * from users where id = 10";
 //        String sql = "select id as u_id, username, age from users";
         ResultSet resultSet = statement.executeQuery(sql);
 
-        while (resultSet.next()) {
-//            resultSet.getXXX()
-            int userId = resultSet.getInt("id");
-//            int userId = resultSet.getInt(1);
-            String username = resultSet.getString(2);
-//            String username = resultSet.getString("username");
-            int age = resultSet.getInt("age");
+        List<User> users = new ArrayList<>();
 
-            System.out.format("user information -> id: %d, username: %s, age: %d", userId, username, age);
-            System.out.println();
+        while (resultSet.next()) {
+            User user = new User();
+
+            user.setId(resultSet.getInt("id"));
+            user.setUsername(resultSet.getString(2));
+            user.setAge(resultSet.getInt("age"));
+
+            users.add(user);
         }
 
+        System.out.println(users);
 
         statement.close();
         connection.close();
