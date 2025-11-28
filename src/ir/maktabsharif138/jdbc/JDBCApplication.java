a62ef9dab3fbe759a36bdeac828390d970936a05
maxixking66 <maxixking66@gamil.com>
2025-11-28 10:40:36 +0330

    feat: add insert query example with generated keys

--- src/ir/maktabsharif138/jdbc/JDBCApplication.java
+++ src/ir/maktabsharif138/jdbc/JDBCApplication.java
@@ -1,8 +1,10 @@
 package ir.maktabsharif138.jdbc;
 
+import ir.maktabsharif138.jdbc.domains.Tag;
 import ir.maktabsharif138.jdbc.util.ApplicationContext;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -13,12 +15,18 @@ public class JDBCApplication {
         Connection connection = ctx.getConnection();
         connection.setAutoCommit(false);
 
+        Tag tag = new Tag();
+        tag.setName("f");
 
         Statement statement = connection.createStatement();
-        int rowsAffected = statement.executeUpdate("delete from users where id = 2");
-        if (rowsAffected == 1) {
-            connection.commit();
+        statement.executeUpdate("insert into tag (name) values ('g')", Statement.RETURN_GENERATED_KEYS);
+        ResultSet resultSet = statement.getGeneratedKeys();
+        if (resultSet.next()) {
+            tag.setId(resultSet.getInt(1));
         }
+        connection.commit();
+
+        System.out.println(tag);
 
         statement.close();
         connection.close();
