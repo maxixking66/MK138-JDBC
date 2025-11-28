51a53291e1dfa145af410540a23f0713afe362f3
maxixking66 <maxixking66@gamil.com>
2025-11-28 10:15:12 +0330

    feat: add update query example

--- src/ir/maktabsharif138/jdbc/JDBCApplication.java
+++ src/ir/maktabsharif138/jdbc/JDBCApplication.java
@@ -1,39 +1,24 @@
 package ir.maktabsharif138.jdbc;
 
-import ir.maktabsharif138.jdbc.domains.User;
 import ir.maktabsharif138.jdbc.util.ApplicationContext;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 public class JDBCApplication {
 
     static void main() throws SQLException {
         ApplicationContext ctx = ApplicationContext.getInstance();
         Connection connection = ctx.getConnection();
+        connection.setAutoCommit(false);
+
 
         Statement statement = connection.createStatement();
-        String sql = "select * from users where id = 10";
-//        String sql = "select id as u_id, username, age from users";
-        ResultSet resultSet = statement.executeQuery(sql);
-
-        List<User> users = new ArrayList<>();
-
-        while (resultSet.next()) {
-            User user = new User();
-
-            user.setId(resultSet.getInt("id"));
-            user.setUsername(resultSet.getString(2));
-            user.setAge(resultSet.getInt("age"));
-
-            users.add(user);
+        int rowsAffected = statement.executeUpdate("update users set age = 30 where id = 2");
+        if (rowsAffected == 1) {
+            connection.commit();
         }
-
-        System.out.println(users);
 
         statement.close();
         connection.close();
