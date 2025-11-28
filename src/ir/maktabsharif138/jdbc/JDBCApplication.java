52963b733440a7ea9bf480092bd85f1f7a0c387d
maxixking66 <maxixking66@gamil.com>
2025-11-28 10:50:26 +0330

    feat: add preparedStatement example

--- src/ir/maktabsharif138/jdbc/JDBCApplication.java
+++ src/ir/maktabsharif138/jdbc/JDBCApplication.java
@@ -4,31 +4,32 @@ import ir.maktabsharif138.jdbc.domains.Tag;
 import ir.maktabsharif138.jdbc.util.ApplicationContext;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class JDBCApplication {
 
     static void main() throws SQLException {
+
+
         ApplicationContext ctx = ApplicationContext.getInstance();
         Connection connection = ctx.getConnection();
-        connection.setAutoCommit(false);
 
-        Tag tag = new Tag();
-        tag.setName("f");
-
-        Statement statement = connection.createStatement();
-        statement.executeUpdate("insert into tag (name) values ('g')", Statement.RETURN_GENERATED_KEYS);
-        ResultSet resultSet = statement.getGeneratedKeys();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from tag where id = ?");
+        preparedStatement.setInt(1, 4);
+        ResultSet resultSet = preparedStatement.executeQuery();
         if (resultSet.next()) {
+            Tag tag = new Tag();
             tag.setId(resultSet.getInt(1));
+            tag.setName(resultSet.getString(2));
+            System.out.println(tag);
+        } else {
+            System.out.println("empty result");
         }
-        connection.commit();
 
-        System.out.println(tag);
 
-        statement.close();
+        preparedStatement.close();
         connection.close();
     }
 }
