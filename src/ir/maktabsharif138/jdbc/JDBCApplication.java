87e8a8cbef90d245793c8ad092805039ee720801
maxixking66 <maxixking66@gamil.com>
2025-12-05 09:31:05 +0330

    add basic example of catching example

--- src/ir/maktabsharif138/jdbc/JDBCApplication.java
+++ src/ir/maktabsharif138/jdbc/JDBCApplication.java
@@ -1,33 +1,26 @@
 package ir.maktabsharif138.jdbc;
 
-import ir.maktabsharif138.jdbc.util.ApplicationContext;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.util.Scanner;
 
 public class JDBCApplication {
 
-    static void main() throws SQLException {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
 
-        ApplicationContext ctx = ApplicationContext.getInstance();
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
 
-//        SQL injection
+        try {
+            divide(first, second);
+        } catch (ArithmeticException e) {
+            System.out.println("handling exception, can not divide by zero");
+        }
 
-        Connection connection = ctx.getConnection();
+        System.out.println("end");
 
-//        String userInput = "sport";
-        String userInput = "'); DROP TABLE tbl_tag; --";
+    }
 
-//        Statement statement = connection.createStatement();
-//
-//        String query = "insert into tbl_tag (name) values ('" + userInput + "')";
-
-        PreparedStatement statement = connection.prepareStatement("insert into tbl_tag (name) values (?)");
-        statement.setString(1, userInput);
-
-        statement.execute();
-
-        ctx.getConnection().close();
+    private static void divide(int first, int second) {
+        System.out.println(first / second);
     }
 }
