bf92fa5d5d96000b6e9481327091aaaea342abbe
maxixking66 <maxixking66@gamil.com>
2025-12-11 10:12:03 +0330

    add example of functional interface with multiple arguments

--- src/ir/maktabsharif138/jdbc/JDBCApplication.java
+++ src/ir/maktabsharif138/jdbc/JDBCApplication.java
@@ -1,42 +1,14 @@
 package ir.maktabsharif138.jdbc;
 
-import java.util.List;
-import java.util.function.Consumer;
-
 public class JDBCApplication {
 
     static void main() {
 
-        List<String> words = List.of("a", "b", "c");
-
-        words.forEach(new ToUppercasePrintConsumer());
-        System.out.println("----------");
-        words.forEach(new Consumer<String>() {
-            @Override
-            public void accept(String s) {
-                System.out.println(s.toUpperCase());
-            }
-        });
-        System.out.println("----------");
-        words.forEach(
-                (varname) -> System.out.println(varname.toUpperCase())
-        );
-        System.out.println("----------");
-        words.forEach(
-                (String myVarName) -> System.out.println(myVarName.toUpperCase())
-        );
-        System.out.println("----------");
-        words.forEach(
-                word -> System.out.println(word.toUpperCase())
-        );
-
-    }
-}
-
-class ToUppercasePrintConsumer implements Consumer<String> {
-
-    @Override
-    public void accept(String myVariableName) {
-        System.out.println(myVariableName.toUpperCase());
+        MyFunctionalInterface myA = (int a, int b) -> {
+            int sum = a + b;
+            System.out.println(sum);
+        };
+        myA.test(10, 20);
+        myA.test(50, 50);
     }
 }
---
+++ src/ir/maktabsharif138/jdbc/MyFunctionalInterface.java
@@ -0,0 +1,6 @@
+package ir.maktabsharif138.jdbc;
+
+public interface MyFunctionalInterface {
+
+    void test(int a, int b);
+}
