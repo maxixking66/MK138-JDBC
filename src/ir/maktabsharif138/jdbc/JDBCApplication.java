a69a5258d25902352e659b4e61c9074c4a27402b
maxixking66 <maxixking66@gamil.com>
2025-12-11 12:03:08 +0330

    add example of supplier and function

--- src/ir/maktabsharif138/jdbc/JDBCApplication.java
+++ src/ir/maktabsharif138/jdbc/JDBCApplication.java
@@ -1,34 +1,152 @@
 package ir.maktabsharif138.jdbc;
 
-import ir.maktabsharif138.jdbc.domains.User;
-import ir.maktabsharif138.jdbc.dto.UserProfileDTO;
-
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 public class JDBCApplication {
 
+    static Map<Integer, Branch> branchMap = new HashMap<>();
+
     static void main() {
 
-        List<User> users = new ArrayList<>();
-        users.add(new User(1));
-        users.add(new User(12));
-        users.add(new User(123));
+//        Consumer
+//        Predicate
+//        Function
+//        Supplier
 
-        List<UserProfileDTO> profileDTOList = new ArrayList<>();
+//        method reference
 
-        Function<User, UserProfileDTO> userToProfileDTOFunction = (User methodInput) -> {
-            UserProfileDTO dto = new UserProfileDTO();
-            dto.setAge(methodInput.getAge());
-            return dto;
-        };
+        List<AccountInfo> accountList = fetchAccounts();
 
-        for (User user : users) {
-            profileDTOList.add(userToProfileDTOFunction.apply(user));
+
+        accountList.forEach(accountInfo -> {
+
+            accountInfo.branch = computeIfAbsent(
+                    accountInfo.branch.id,
+//                    id -> getById(id)
+                    JDBCApplication::getById
+            );
+
+        });
+
+//        fill branch information
+
+        System.out.println(accountList);
+    }
+
+    static Branch computeIfAbsent(Integer key, Function<Integer, Branch> mappingFunction) {
+        Objects.requireNonNull(mappingFunction);
+        Branch v;
+        if ((v = branchMap.get(key)) == null) {
+            Branch newValue;
+            if ((newValue = mappingFunction.apply(key)) != null) {
+                branchMap.put(key, newValue);
+                return newValue;
+            }
         }
 
-        System.out.println(profileDTOList);
+        return v;
+    }
 
+    static Branch getOrDefault(Branch branch, BranchSupplier supplier) {
+        if (branch != null) {
+            return branch;
+        }
+        return supplier.get();
+    }
+
+    static Branch getById(Integer branchId) {
+        System.out.println("invoking very slow api for branch: " + branchId);
+        Branch branch = new Branch();
+        branch.id = branchId;
+        branch.code = branchId + "";
+        branch.name = branchId + "";
+        return branch;
+    }
+
+
+    private static List<AccountInfo> fetchAccounts() {
+        return List.of(
+                new AccountInfo(
+                        "1",
+                        new Branch(1)
+                ),
+                new AccountInfo(
+                        "2",
+                        new Branch(2)
+                ),
+                new AccountInfo(
+                        "3",
+                        new Branch(1)
+                ),
+                new AccountInfo(
+                        "4",
+                        new Branch(4)
+                ),
+                new AccountInfo(
+                        "5",
+                        new Branch(2)
+                )
+        );
+    }
+}
+
+class AccountInfo {
+    String accountNumber;
+    Branch branch;
+
+    public AccountInfo(String accountNumber, Branch branch) {
+        this.accountNumber = accountNumber;
+        this.branch = branch;
+    }
+
+    @Override
+    public String toString() {
+        return "AccountInfo{" +
+               "accountNumber='" + accountNumber + '\'' +
+               ", branch=" + branch +
+               '}';
+    }
+}
+
+class Branch {
+    Integer id;
+    String code;
+    String name;
+
+    public Branch() {
+    }
+
+    public Branch(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Branch{" +
+               "id=" + id +
+               ", code='" + code + '\'' +
+               ", name='" + name + '\'' +
+               '}';
+    }
+}
+
+class BranchSupplier {
+    int branchId;
+
+    BranchSupplier(int branchId) {
+        this.branchId = branchId;
+    }
+
+    Branch get() {
+        System.out.println("invoking very slow api for branch: " + branchId);
+        Branch branch = new Branch();
+        branch.id = branchId;
+        branch.code = branchId + "";
+        branch.name = branchId + "";
+        return branch;
     }
 }
