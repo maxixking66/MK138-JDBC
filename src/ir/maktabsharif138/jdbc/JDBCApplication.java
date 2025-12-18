e57c574c51d770e058b97fdab546fd765890a133
maxixking66 <maxixking66@gamil.com>
2025-12-18 09:41:05 +0330

    add parameter method references example 1

--- src/ir/maktabsharif138/jdbc/JDBCApplication.java
+++ src/ir/maktabsharif138/jdbc/JDBCApplication.java
@@ -1,16 +1,35 @@
 package ir.maktabsharif138.jdbc;
 
 import ir.maktabsharif138.jdbc.util.LastIndexFinder;
-import ir.maktabsharif138.jdbc.util.StringUtils;
 
 public class JDBCApplication {
 
     static void main() {
 
-//        LastIndexFinder finder = (first, second) -> {
-//            return StringUtils.findLastIndexOf(second, first);
-//        };
-        LastIndexFinder finder = StringUtils::findLastIndexOf;
+        LastIndexFinder finder = (String first, String second) -> {
+            return first.lastIndexOf(second);
+        };
+        LastIndexFinder ssss = String::compareTo;
+
+        A a = (B b, C c) -> {
+            return b.logic(c);
+        };
+
+        A a1 = B::logic;
+
 
     }
+}
+
+interface A {
+    float test(B b, C c);
+}
+
+class B {
+    float logic(C c) {
+        return 58.5f;
+    }
+}
+
+class C {
 }
--- src/ir/maktabsharif138/jdbc/util/LastIndexFinder.java
+++ src/ir/maktabsharif138/jdbc/util/LastIndexFinder.java
@@ -1,7 +1,9 @@
 package ir.maktabsharif138.jdbc.util;
 
+import java.io.UnsupportedEncodingException;
+
 @FunctionalInterface
 public interface LastIndexFinder {
 
-    int find(String f, String e);
+    int find(String f, String e) throws UnsupportedEncodingException;
 }
