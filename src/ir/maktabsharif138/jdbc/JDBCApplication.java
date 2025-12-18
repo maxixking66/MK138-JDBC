75fadd7e50e1f1329dd604abd395ffd737bdc3de
maxixking66 <maxixking66@gamil.com>
2025-12-18 10:16:07 +0330

    add first stream API example

--- src/ir/maktabsharif138/jdbc/JDBCApplication.java
+++ src/ir/maktabsharif138/jdbc/JDBCApplication.java
@@ -1,23 +1,41 @@
 package ir.maktabsharif138.jdbc;
 
-import ir.maktabsharif138.jdbc.util.StringUtils;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class JDBCApplication {
 
-    static StringUtils stInstance = new StringUtils();
-
-
     static void main() {
 
-        CharToStringFactory factory = (chars) -> {
-            return new String(chars);
-        };
+        List<Integer> numbers = getRandomNumbers();
 
-        CharToStringFactory factory1 = String::new;
+        List<Integer> filteredNumbers = new ArrayList<>();
 
+        numbers.forEach(num -> {
+            if (num % 2 == 0) {
+                filteredNumbers.add(num);
+            }
+        });
+        System.out.println(filteredNumbers);
+
+//        numbers.stream().filter(num -> {
+//            if (num % 2 == 0) {
+//                return true;
+//            }
+//            return false;
+//        })
+        Stream<Integer> stream = numbers.stream();
+        List<Integer> filteredList = stream
+                .filter(num -> num % 2 == 0)
+                .toList();
+
+        System.out.println(filteredList);
     }
-}
 
-interface CharToStringFactory {
-    String create(char[] chars);
+    private static List<Integer> getRandomNumbers() {
+        return List.of(
+                1, 2, 3, 4, 6, 8, 9
+        );
+    }
 }
