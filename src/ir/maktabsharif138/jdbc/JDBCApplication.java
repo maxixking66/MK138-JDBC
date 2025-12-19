a956dc974eab845d8b5a58829142b28faf73dd7b
maxixking66 <maxixking66@gamil.com>
2025-12-19 11:47:39 +0330

    add wildcards generic type boundary

--- src/ir/maktabsharif138/jdbc/JDBCApplication.java
+++ src/ir/maktabsharif138/jdbc/JDBCApplication.java
@@ -1,13 +1,63 @@
 package ir.maktabsharif138.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JDBCApplication {
 
     static void main() {
+        List<A> aList = new ArrayList<A>();
+
+        List<B> bList = new ArrayList<B>();
+
+        process(aList);
+
+        process(bList);
+
+        printElementsOfCollection(aList);
+        printElementsOfCollection(bList);
 
     }
 
-    public static void put(String key, Long value) {
-
+    static void printElementsOfCollection(List<?> objects) {
+        for (Object object : objects) {
+            System.out.println(object);
+        }
     }
+
+    static void process(List<? extends A> aList) {
+        for (A a : aList) {
+            a.test();
+        }
+    }
+
+    static void insert(List<? super A> aList) {
+        aList.add(new A());
+        aList.add(new B());
+        aList.add(new C());
+
+        Object object = aList.get(0);
+    }
+
+//    static void process(List<? extends A> list) {
+//        for (A a : list) {
+//            a.test();
+//        }
+//    }
+}
+
+class X {
+
+}
+
+class A extends X {
+    public void test() {
+    }
+}
+
+class B extends A {
+}
+
+class C extends A {
 }
 
